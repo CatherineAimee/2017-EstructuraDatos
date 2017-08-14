1f0b523be662c19f9c72cb113c79637ba2d23965
Catherine <cathyhdezoso9821@gmail.com>
2017-08-14 11:18:44 -0500

    Practica 01

---
+++ src/practica01/Administrativo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica01;
+
+/**
+ *
+ * @author Neil
+ */
+public interface Administrativo {
+
+	double BONO_MAXIMO = 10000;
+	
+	String administrar();
+	String definirAumentos();
+	String reportarLogros();	
+}
